5035f7f30683a2ade6f7aba7bf9ad8685f402084
99daycoder <sambaeverything@gmail.com>
2024-02-17 20:45:32 +0000

    First commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            Footer FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter FdfdFooter Fdfd
+        </div>
+    )
+}
+
+export default Footer
